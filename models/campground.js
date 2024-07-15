38602c2357d0e9447a17df46ec4c2932f1656723
skylarshi123 <skylarshi123@gmail.com>
2024-07-14 23:10:13 -0700

    Added Review Form

--- models/campground.js
+++ models/campground.js
@@ -1,3 +1,4 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -6,7 +7,13 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema)
