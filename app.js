52b9a3823b53a748d3d7bbab5588e2283de562f3
skylarshi123 <skylarshi123@gmail.com>
2024-07-16 15:58:20 -0700

    Configured Session

--- app.js
+++ app.js
@@ -3,15 +3,13 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
-const Joi = require("joi");
-const {campgroundSchema, reviewSchema} = require("./schemas.js");
-const Campground = require("./models/campground");
-const Review = require("./models/review");
+const session = require("express-session");
+
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
+
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {});
 
@@ -30,7 +28,17 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-
+const sessionConfig = {
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly : true,
+    expires: Date.now() + 1000 * 60 *60 * 24 * 7,
+    maxAge: 1000 * 60 *60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig))
 
 
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const {campgroundSchema} = require("../schemas.js");
+
 const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 
