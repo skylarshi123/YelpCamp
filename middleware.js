00d189b59cba5f3e866167b7d27ff2590eef78f3
skylarshi123 <skylarshi123@gmail.com>
2024-07-17 16:04:37 -0700

    Reviews Authorization - delete only if you are the creator

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const {campgroundSchema, reviewSchema} = require("./schemas.js");
 const ExpressError = require("./utils/ExpressError");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 
 module.exports.isLoggedIn = (req, res, next) => {
@@ -40,6 +41,17 @@ module.exports.isAuthor = async(req, res, next) => {
       }
       next();
 }
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const { reviewId, id } = req.params;
+    const campground = await Review.findById(reviewId);
+        if(!campground.author.equals(req.user._id)){
+          req.flash('error', 'You do not have permission to do that!');
+          return res.redirect(`/campgrounds/${id}`);
+        }
+        next();
+  }
+
 module.exports.validateReview = (req, res, next) => {
     const {error} = reviewSchema.validate(req.body);
     if (error) {
