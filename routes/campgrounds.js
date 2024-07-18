c9606cde3821679e886dec66cb7284eed78aaff1
skylarshi123 <skylarshi123@gmail.com>
2024-07-18 00:17:42 -0700

    Use multer cloudinary npm docs, and uploading to Cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require('../cloudinary');
+const upload = multer({ storage});
+
 
 
 const Campground = require("../models/campground");
