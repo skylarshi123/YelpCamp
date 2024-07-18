5dbb08805b845c399d05537e234ce6a369c38c09
skylarshi123 <skylarshi123@gmail.com>
2024-07-18 00:39:37 -0700

    Storing Uploaded Image Links in Mongo

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,11 +14,7 @@ const campgrounds = require('../controllers/campgrounds');
 
 router.route('/')
   .get(catchAsync(campgrounds.index))
-//  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-  .post(upload.array('image'), (req, res) => {
-    console.log(req.body, req.files);
-    res.send("It worked");
-  });
+ .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 router.route('/:id')
